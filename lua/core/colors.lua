7c9ca7a2fc89720f5213b1765ea9da50a1da1dbb
Левков Глеб <kiss-shot-heart-under-blade@yandex.by>
2023-12-25 15:32:48 +0300

    fix: gitsigns bg none

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -11,6 +11,9 @@ function SetColor(color)
     vim.api.nvim_set_hl(0, "LineNr", {bg = "none"})
     vim.api.nvim_set_hl(0, "SignColumn", {bg = "none"})
     vim.api.nvim_set_hl(0, "FloatBorder", {bg = "none"})
+    vim.api.nvim_set_hl(0, "GitSignsAdd", {bg = "none"})
+    vim.api.nvim_set_hl(0, "GitSignsChange", {bg = "none"})
+    vim.api.nvim_set_hl(0, "GitSignsDelete", {bg = "none"})
 end
 
 SetColor('kanagawa')
