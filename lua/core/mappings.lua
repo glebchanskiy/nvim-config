502fc03437cf6e1b68a4667000b056176e1021c4
Левков Глеб <kiss-shot-heart-under-blade@yandex.by>
2023-12-26 19:05:12 +0300

    feat: update mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -5,3 +5,18 @@ vim.keymap.set('n', '<leader>/', ':CommentToggle<CR>')
 -- Neo Tree
 vim.keymap.set('n', '<leader>e', ':Neotree float focus<CR>')
 vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
+
+-- Tabs
+vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+
+-- Terminal
+vim.keymap.set('n', '<leader>tf', ':ToggleTerm direction=float<CR>')
+vim.keymap.set('n', '<leader>th', ':ToggleTerm direction=horizontal<CR>')
+vim.keymap.set('n', '<leader>tv', ':ToggleTerm direction=vertical size=40<CR>')
+
+-- Other
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
